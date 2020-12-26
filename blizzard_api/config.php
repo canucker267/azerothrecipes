cc325e91d37efbf0399d3d87a38aebe3a7e9cd4a
canucker267 <58878858+canucker267@users.noreply.github.com>
2020-12-26 01:16:17 -0500

    Update config.php

--- blizzard_api/config.php
+++ blizzard_api/config.php
@@ -6,9 +6,9 @@ class Config
 {
   const VERSION = '1.0.0';
 
-  public static $apiKey;
-  public static $apiSecret;
-  public static $redirectURI;
+  public static $apiKey = 'e6ea0ca3adbb46e691dcafc87ed8daeb';
+  public static $apiSecret = 'v40DzzrJswGv26jcBLRgwe428VXBi2Qy';
+  public static $redirectURI = 'https://canucker267.github.io/wowrecipes/';
 
   public static $storeAccessTokenInSession = true;
   public static $accessTokenSessionKey = 'blizzard_api_access_token';
