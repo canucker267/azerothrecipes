2310c65d62de820e4e61c9d373d4af6eadfaad2d
canucker267 <58878858+canucker267@users.noreply.github.com>
2020-12-26 01:08:23 -0500

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once "blizzard_api/blizzard_api.php";
-$apiClient = new BlizzardAPI\Wow\Wow('us');
-$character = $apiClient->character()->get('blades-edge', 'canucker', ['locale'=>'en_US']);
-?>
